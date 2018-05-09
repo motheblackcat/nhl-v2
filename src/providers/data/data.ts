857c9433c5bb2fa7cc11188fd77263d7eaaeeaaa
Mo <morad.elhamdouni@gmail.com>
2018-05-09 20:58:09 +0200

    quick update

--- src/providers/data/data.ts
+++ src/providers/data/data.ts
@@ -1,12 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
 
-/*
-  Generated class for the DataProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class DataProvider {
   checked: boolean;
@@ -24,7 +18,6 @@ export class DataProvider {
     localStorage.setItem(key, JSON.stringify(data));
   }
 
-  // REFACTOR THIS METHOD?
   getData(array: Array<any>, dataKey: string) {
     const data = JSON.parse(localStorage.getItem(dataKey));
     if (data !== null) {

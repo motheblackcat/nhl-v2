f78e4cf23dadb41e263396cd055adfba5982fe51
motheblackcat <morad.elhamdouni@gmail.com>
2020-09-13 14:24:21 +0200

    Fix sub menu icons

--- src/app/shared/fab/fab.component.ts
+++ src/app/shared/fab/fab.component.ts
@@ -19,10 +19,11 @@ export class FabComponent implements OnInit {
   constructor(private router: Router, private modalController: ModalController) {}
 
   ngOnInit() {
+    this.display = false;
     this.router.config.filter(route => route.data).forEach(route => this.routes.push({ path: `/${route.path}`, icon: route.data.icon }));
     this.router.events.subscribe(event => {
       if (event instanceof RoutesRecognized) {
-        this.display = event.url !== '/persona-select';
+        this.display = event.url !== '/persona';
         const currentRoute = this.routes.find(route => route.path === event.url);
         this.icon = currentRoute ? currentRoute.icon : '';
       }

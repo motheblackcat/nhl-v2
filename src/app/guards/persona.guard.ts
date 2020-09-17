9146c208d644842d922103774560ec0ca71eb964
motheblackcat <morad.elhamdouni@gmail.com>
2020-09-17 15:14:57 +0200

    Fix persona guard

--- src/app/guards/persona.guard.ts
+++ src/app/guards/persona.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { CanActivate, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { PersonaService } from '../services/persona.service';
 
@@ -8,12 +8,9 @@ import { PersonaService } from '../services/persona.service';
 })
 export class PersonaGuard implements CanActivate {
   constructor(private router: Router, public personaService: PersonaService) {}
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  canActivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     if (!this.personaService.currentPersona) {
-      this.router.navigate(['/select-persona']);
+      this.router.navigate(['/persona']);
     }
     return !!this.personaService.currentPersona;
   }

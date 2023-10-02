60b60af92556172be3de7755f5c13de72b07baad
motheblackcat <morad.elhamdouni@gmail.com>
2023-10-02 09:50:45 +0200

    Update @angular/core

--- src/app/guards/persona.guard.ts
+++ src/app/guards/persona.guard.ts
@@ -1,14 +1,14 @@
 import { Observable } from 'rxjs';
 
 import { Injectable } from '@angular/core';
-import { CanActivate, Router, UrlTree } from '@angular/router';
+import { Router, UrlTree } from '@angular/router';
 
 import { PersonaService } from '../services/persona.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PersonaGuard implements CanActivate {
+export class PersonaGuard  {
   constructor(private router: Router, public personaService: PersonaService) { }
   canActivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     if (!this.personaService.currentPersona) {
